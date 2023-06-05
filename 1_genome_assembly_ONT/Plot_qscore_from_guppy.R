a733423e2f2706e00b88553c64b234167d69189b
kaede0e <91504464+kaede0e@users.noreply.github.com>
2023-06-05 15:58:33 -0700

    Add files via upload

---
+++ 1_genome_assembly_ONT/Plot_qscore_from_guppy.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(ggplot2)
+
+setwd("/Users/hirabayashikaede/Documents/UVic/Masters/Lingonberry data/Sequencing/Lingonberry_Takara_04_SREXS")
+setwd("/Users/hirabayashikaede/Documents/UVic/Masters/Lingonberry data/Sequencing/Lingonberry_minus_Takara_01_SREXS")
+setwd("/Users/hirabayashikaede/Documents/UVic/Masters/Lingonberry data/Sequencing/Lingonberry_minus_Takara_02_SRE")
+setwd("/Users/hirabayashikaede/Documents/UVic/Masters/Lingonberry data/Sequencing/duplex_reads")
+
+qscore <- read_table("Lingonberry_minus_Takara_02_nsSRE_01_4_Dec16_2022_duplex_basecalled_stats.txt")
+qscore %>%
+  ggplot()+
+  geom_density(aes(x=mean_qscore_template))
+
+qscore %>%
+  filter(sequence_length_template >= 10000) %>%
+  ggplot()+
+  geom_histogram(aes(x=sequence_length_template))
+
+#Get N50 and Qscore mean for raw reads that guppy basecalled
+qscore %>%
+  summarise(mean(mean_qscore_template))
+qscore <- qscore[order(qscore$sequence_length_template, decreasing= TRUE),]
+qscore$cum_sum<- cumsum(qscore$sequence_length_template)
+qscore[qscore$cum_sum > max(qscore$cum_sum)/2,][1,] #length corresponds to N50
+
+#How much data produced 
+qscore %>% filter(mean_qscore_template>=10)%>%
+  summarise(data = sum(sequence_length_template))%>% 
+  mutate(`data(Gb)`= data / 1000000000)
+  
+#Guppy basecaller pass/fail
+qscore <- read_table("minus_Takara_02_nsSRE_01_2_Dec12_2022_sup_qc_data.txt")
+qscore %>% 
+  group_by(filename)%>% 
+  count
+  ggplot()+
+  geom_histogram(aes(x=mean_qscore_template, 
+                     fill = passes_filtering, colour = passes_filtering))
+  
+  
