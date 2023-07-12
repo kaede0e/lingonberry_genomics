865d6318bb1585489bcb2976bee3f39324e05d88
kaede0e <91504464+kaede0e@users.noreply.github.com>
2023-07-12 11:52:13 -0700

    Update and rename popgen_msmc2.R to popgen_msmc2_psmc.R

--- 2_popgen_subspecies/popgen_msmc2_psmc.R
+++ 2_popgen_subspecies/popgen_msmc2_psmc.R
@@ -60,3 +60,31 @@ plot(crossPopDat$left_time_boundary/mu*gen, 2 * crossPopDat$lambda_01 / (crossPo
 lines(crossPopDat$left_time_boundary/mu*gen, 2 * crossPopDat$lambda_01 / (crossPopDat$lambda_00 + crossPopDat$lambda_11), type="s")
 #the single-individual (only using offspring haplotypes) resulted in x10 older estimate
 
+#PSMC results 
+minusDat_psmc<-read.table("popgen_results/Lingonberry_minus_5yr_psmc.0.txt", header=FALSE) %>%
+  as_tibble()%>%
+  rename(Time = V1, 
+         Ne_tenthousands = V2)%>% 
+  mutate(Ne = Ne_tenthousands * 10000, 
+         ssp = "minus")%>% 
+  select(Time, Ne, ssp)
+RedCandyDat_psmc<-read.table("popgen_results/Lingonberry_RedCandy_5yr_psmc_2.0.txt", header=FALSE) %>%
+  as_tibble()%>%
+  rename(Time = V1, 
+         Ne_tenthousands = V2)%>% 
+  mutate(Ne = Ne_tenthousands * 10000, 
+         ssp = "RedCandy")%>% 
+  select(Time, Ne, ssp)
+LingonberryDat_psmc <- rbind(minusDat_psmc, RedCandyDat_psmc)
+
+LingonberryDat_psmc %>% 
+  ggplot()+
+  geom_step(aes(Time, Ne, colour = ssp))+
+  scale_x_log10(limits = c(50000,11000000))+
+  scale_y_log10()+
+  theme_classic()+
+  theme(legend.position = "na")+
+  scale_colour_manual(values = c(LGpal[1], LGpal[11]))+
+  xlab("Years ago (g=5, u=0.3e10^-8)")+ ylab("Effective population size")
+
+
