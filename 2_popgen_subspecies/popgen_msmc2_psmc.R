68685b79dbf09a9feaa0e5fc004dcc57d7d7a5f6
kaede0e <91504464+kaede0e@users.noreply.github.com>
2023-08-09 14:16:49 -0700

    Update popgen_msmc2_psmc.R

--- 2_popgen_subspecies/popgen_msmc2_psmc.R
+++ 2_popgen_subspecies/popgen_msmc2_psmc.R
@@ -3,22 +3,6 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 
-#--Github MSMC2 examples with human dataset------------------------------
-mu <- 1.25e-8
-gen <- 30
-afrDat<-read.table("popgen_results/NA12878.chr1.msmc2.final.txt", header=TRUE)#single individual 
-eurDat<-read.table("popgen_results/NA19240.chr1.msmc2.final.txt", header=TRUE)#single individual
-afrDat<-read.table("popgen_results/AFR.msmc2.final.txt", header=TRUE)
-eurDat<-read.table("popgen_results/EUR.msmc2.final.txt", header=TRUE)
-plot(afrDat$left_time_boundary/mu*gen, (1/afrDat$lambda)/(mu), log="x",ylim=c(0,100000),
-     type="n", xlab="Years ago", ylab="effective population size")
-lines(afrDat$left_time_boundary/mu*gen, (1/afrDat$lambda)/(mu), type="s", col="red")#the tutorial is apparently wrong; with 2*mu 
-lines(eurDat$left_time_boundary/mu*gen, (1/eurDat$lambda)/(mu), type="s", col="blue")
-legend("topright",legend=c("African", "European"), col=c("red", "blue"), lty=c(1,1))
-crossPopDat<-read.table("EUR_AFR.combined.msmc2.final.txt", header=TRUE)
-plot(crossPopDat$left_time_boundary/mu*gen, 2 * crossPopDat$lambda_01 / (crossPopDat$lambda_00 + crossPopDat$lambda_11),
-     xlim=c(1000,500000),ylim=c(0,1), type="n", xlab="Years ago", ylab="relative cross-coalescence rate")
-lines(crossPopDat$left_time_boundary/mu*gen, 2 * crossPopDat$lambda_01 / (crossPopDat$lambda_00 + crossPopDat$lambda_11), type="s")
 
 #--Lingonberry dataset------------------------------
 mu <- 3e-9 #mutation rate in Arabidopsis thaliana
